e929c9b902cbcf18735ed9781be9c8b4f5387649
Shawn Tompke <shawntompke@Shawns-MacBook-Pro.local>
2020-07-28 10:12:12 -0700

    starting problem.

---
+++ main.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/ctci-bubble-sort/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+
+# Sorting: Bubble Sort
+
+"""
+UPER:
+U:
+    > 
+
+"""
